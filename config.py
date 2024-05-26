31c31db3e3045db75165ce5425b5b8b91716b2e5
A Jakhar <125469531+jakharamit44@users.noreply.github.com>
2024-05-26 16:40:49 +0530

    Update config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-{
-    "api_id": 17737898, // Put your API ID here
-    "api_hash": "ad762fe0516f367115ba651d929cf429", // Put your API Hash here
-    "admin": 5076949930, // Put your Admin Telegram user ID here
-    "auto_upgrade": true, // Set to true to enable auto-upgrade
-    "max_charge_level": 3, // Set maximum charge level
-    "max_energy_level": 8, // Set maximum energy level
-    "max_tap_level": 8 // Set maximum tap level
-}
